3738cfe0cdbe142c751dcd8b47d98b0a8d33316a
jorgesdr <jorge1439@hotmail.com>
2024-12-04 15:19:45 +0100

    Implement language toggle functionality and update footer structure

--- index.js
+++ index.js
@@ -9,22 +9,32 @@ function closeLightbox() {
 
 
 
-const btnEs = document.getElementById("eslanguaje");
-const btnEn = document.getElementById("enlanguaje");
-const enContent = document.getElementById('en');
-const esContent = document.getElementById('es');
-
-btnEn.addEventListener('click', () => {
-    enContent.classList.remove('hidden');
-    esContent.classList.add('hidden');
+document.addEventListener("DOMContentLoaded", function () {
+    const esLanguaje = document.getElementById("esLanguaje");
+    const enLanguaje = document.getElementById("enLanguaje");
+    // Aquí va el código que usa esLanguaje y enLanguaje
+    const esText = document.querySelectorAll('#es');
+    const enText = document.querySelectorAll('#en');
+    function setLanguage(language) {
+      if (language === 'es') {
+        esLanguaje.classList.add('active');
+        enLanguaje.classList.remove('active');
+        esText.forEach(el => el.classList.remove('hidden'));
+        enText.forEach(el => el.classList.add('hidden'));
+      } else if (language === 'en') {
+        enLanguaje.classList.add('active');
+        esLanguaje.classList.remove('active');
+        esText.forEach(el => el.classList.add('hidden'));
+        enText.forEach(el => el.classList.remove('hidden'));
+      }
+    }
+    console.log(esLanguaje);
+    esLanguaje.addEventListener('click', () => setLanguage('es'));
+    enLanguaje.addEventListener('click', () => setLanguage('en'));
+    
+    // Establecer el idioma por defecto (puedes cambiarlo a 'es' si prefieres español como predeterminado)
+    setLanguage('es');
 });
 
-// Función para mostrar español y ocultar inglés
-btnEs.addEventListener('click', () => {
-    esContent.classList.remove('hidden');
-    enContent.classList.add('hidden');
-});
 
-document.addEventListener('DOMContentLoaded', () => {
-    enContent.classList.add('hidden'); // Mostrar español por defecto
-})
+// Eventos de clic para cambiar de idioma
